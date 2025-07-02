74cf91d625f9776964b792d779813b971d71b274
Anna Holzer <r.holzer089@gmail.com>
2025-07-02 10:56:35 +0000

    flash card for learning

--- src/pages/FlashCards.jsx
+++ src/pages/FlashCards.jsx
@@ -1,7 +1,23 @@
+import alphabet from "../assets/alphabet.json"
+import FlashCard from "../components/FlashCard"
+import style from "../style/flashcards.module.scss"
+
 export default function FlashCards() {
     return (
-        <h1>
-            FlashCards
-        </h1>
+        <>
+            <h1>
+                FlashCards
+            </h1>
+
+            <article className={style.flashcards}>
+
+                {
+                    alphabet.alphabet.map(({ letter, word }, index) => {
+                        return <FlashCard key={index} letter={letter} word={word} />
+                    })
+                }
+
+            </article>
+        </>
     )
 }
