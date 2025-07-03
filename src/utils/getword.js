32c4e3bf93522a44b0ac6cd8a6b3ba879cb3be12
Anna Holzer <r.holzer089@gmail.com>
2025-07-03 08:24:09 +0000

    fix getword function

---
+++ src/utils/getword.js
@@ -0,0 +1,12 @@
+import alphabet from "../assets/alphabet.json"
+
+
+
+export function getword(letter) {
+
+    letter = letter.toUpperCase()
+
+    const entryForLetter = alphabet.alphabet.find(entry => entry.letter === letter);
+    return entryForLetter ? entryForLetter.word : null;
+}
+
