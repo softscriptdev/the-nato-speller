6902ede89cbb60df5fb6641b60fca702527c380a
Anna Holzer <r.holzer089@gmail.com>
2025-07-23 14:56:29 +0200

    deploy error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/the-nato-speller/",
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
-})
+});
