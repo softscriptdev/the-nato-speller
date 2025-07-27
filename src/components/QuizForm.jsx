3994772e00cd1b6b1a6850cd0bd7b0444e84055f
Anna Holzer <r.holzer089@gmail.com>
2025-07-27 15:04:03 +0200

    redesign quiz

--- src/components/QuizForm.jsx
+++ src/components/QuizForm.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 import QuizHeader from "./QuizHeader";
 import { getword } from "../utils/getword";
 import geticon from "../utils/geticon";
@@ -10,6 +10,8 @@ export default function QuizForm({
   style,
   setquiztext,
 }) {
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+
   /* NOTE letters of the text */
   const letters = getquiztext.split("");
 
@@ -35,6 +37,20 @@ export default function QuizForm({
     }
   };
 
+  useEffect(() => {
+    let count = 0;
+    letters.forEach((_letter, index) => {
+      if (natoWords[index] === "(space)") count++;
+      else if (
+        natoWords[index] === userInput[index] &&
+        userInput[index] !== ""
+      ) {
+        count++;
+      }
+    });
+    setCorrectAnswers(count);
+  }, [userInput, natoWords, letters]);
+
   return (
     <section>
       <QuizHeader
@@ -44,8 +60,20 @@ export default function QuizForm({
         setquiztext={setquiztext}
       />
 
+      <section
+        className={style.quizresult}
+        style={{ "--percent": `${(correctAnswers / natoWords.length) * 100}%` }}
+      >
+        <p>
+          You got {correctAnswers} out of {natoWords.length} words right, that's
+          {" " + (correctAnswers / natoWords.length) * 100}%.
+        </p>
+      </section>
+
       <article className={style.quizsection}>
         {letters.map((letter, index) => {
+          const isCorrect = natoWords[index] === userInput[index];
+
           return (
             <>
               {natoWords[index] == "(space)" ? (
@@ -59,7 +87,7 @@ export default function QuizForm({
                     size="2x"
                     key={`icon-${index}`}
                   />
-                  {natoWords[index] == userInput[index] ? (
+                  {isCorrect ? (
                     <p key={`text-${index}`}>{natoWords[index]}</p>
                   ) : (
                     <input
