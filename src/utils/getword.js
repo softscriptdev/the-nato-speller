bf9a79f1214c2433ba361d51345a715bb2d82362
Anna Holzer <r.holzer089@gmail.com>
2025-07-30 13:32:45 +0200

    #2 Put unknown characters in ?CHARACTER?

--- src/utils/getword.js
+++ src/utils/getword.js
@@ -7,6 +7,6 @@ export function getword(letter) {
     letter = letter.toUpperCase()
 
     const entryForLetter = alphabet.alphabet.find(entry => entry.letter === letter);
-    return entryForLetter ? entryForLetter.word : "??";
+    return entryForLetter ? entryForLetter.word : `?${letter}?`;
 }
 
