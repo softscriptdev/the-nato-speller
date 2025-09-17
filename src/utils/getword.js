b22461b7ae68f503c461c711d162bfe38b8cb0a8
Raphael Holzer <info@digitaleweltlibrary.at>
2025-09-17 12:10:06 +0200

    #6 Characters that are not letters or numbers are marked with (unknown: ???)

--- src/utils/getword.js
+++ src/utils/getword.js
@@ -7,6 +7,6 @@ export function getword(letter) {
     letter = letter.toUpperCase()
 
     const entryForLetter = alphabet.alphabet.find(entry => entry.letter === letter);
-    return entryForLetter ? entryForLetter.word : `?${letter}?`;
+    return entryForLetter ? entryForLetter.word : `(unknown: ${letter})`;
 }
 
